ebe07956e3a3b0cc93ebe27baba3876096f139ff
Milad Jafari <milad.jafary@gmail.com>
2019-12-03 21:23:22 +0330

    Adding create and update form.

--- src/main/resources/META-INF/resources/app/app.js
+++ src/main/resources/META-INF/resources/app/app.js
@@ -4,7 +4,55 @@ angular
     templateUrl: 'app/stock-list/stock-list.template.html',
     controller: function StockListController($http) {
         var self = this;
-        self.alert={
+        var handleSuccess = function(message) {
+            self.alert.reset();
+            self.alert.show = true;
+            self.alert.type = 'success';
+            self.alert.messages.push(message)
+            self.refresh();
+
+            self.form.reset();
+            self.form.hide();
+        };
+
+        var onError = function(response){
+            var errors = response.data;
+
+            self.alert.reset();
+            self.alert.type = 'danger';
+            self.alert.show = true;
+            errors.forEach(function(error){
+                self.alert.messages.push(error.description);
+            })
+        }
+
+        self.form= {
+            title: '',
+            isShow: false,
+            stockId : null,
+            stockName: {
+                value: null,
+                readonly: false
+            },
+            stockPrice: null,
+            hide: function() {
+                this.isShow = false;
+            },
+            show: function() {
+                this.isShow = true;
+            },
+            reset: function() {
+                this.stockId = null;
+                this.title = '';
+                this.stockName.value = null;
+                this.stockName.readonly= false;
+                this.stockPrice = null;
+
+                this.hide();
+            }
+        };
+
+        self.alert= {
             type: 'success',
             show: false,
             messages : [],
@@ -33,9 +81,47 @@ angular
                 })
         }
 
+        self.closeForm =function () {
+            self.alert.reset();
+            self.form.reset();
+        }
+        self.save = function() {
+            if (self.form.stockId == null){
+                $http.post('/api/stocks/',{
+                    name: self.form.stockName.value,
+                    price: self.form.stockPrice
+                }).then(function onSuccess(response){handleSuccess('Stock has been successfully created');}, onError);
+
+            } else {
+                $http.put('/api/stocks/' + self.form.stockId, {price: self.form.stockPrice})
+                    .then(function onSuccess(response){handleSuccess('Stock has been successfully updated');}, onError);
+            }
+        }
+
+        self.createNewStock = function() {
+            self.alert.reset();
+            self.form.reset();
+
+            self.form.title = 'New Stock';
+            self.form.show();
+        }
+
+        self.updateStock = function(stock) {
+            self.alert.reset();
+            self.form.reset();
+
+            self.form.title = 'Update Stock';
+            self.form.stockId = stock.id;
+            self.form.stockName.value = stock.name;
+            self.form.stockName.readonly = true;
+            self.form.stockPrice = stock.price;
+
+            self.form.show();
+        }
+
         self.refresh = function() {
+            self.stocks=[];
             $http.get('/api/stocks').then(function(response){
-                console.log (response.data);
                 self.stocks = response.data;
             })
         };
