ef673022972e4430de68e7fd2f6ab6ba65470377
Milad Jafari <milad.jafary@gmail.com>
2020-01-02 11:43:40 +0330

    Removing unnecessary fields

--- src/main/java/com/miladjafari/dao/StockDao.java
+++ src/main/java/com/miladjafari/dao/StockDao.java
@@ -1,23 +1,14 @@
 package com.miladjafari.dao;
 
-import com.miladjafari.dto.StockDto;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.validation.Validator;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @ApplicationScoped
 public class StockDao implements PanacheRepositoryBase<Stock,Long> {
-    private List<StockDto> stocks = new ArrayList<>();
-
-    @Inject
-    Validator validator;
 
     public Optional<Stock> findByName(String name) {
         return Optional.ofNullable(Stock.find("name", name).firstResult());
